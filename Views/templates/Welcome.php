f8b5570b98fe225ad1ed5c9f165e31038ffe78b7
root <root@becarios-2.novalocal>
2023-04-10 14:59:07 +0000

    folders

--- Views/templates/Welcome.php
+++ Views/templates/Welcome.php
@@ -1,7 +1,7 @@
 <form action="<?php base_url();?>/vacation">
-      
+    </br>  
     <div class="container">
-    <h1>Welcome to the Simple Travel Agency!</h1>
+    <h1>elcome to the Simple Travel Agency!</h1>
     <p>The is a sample site you can test with BlazeMeter! </p>
     <p>Check out our <a href="vacation">DESTINATION OF THE WEEK! THE BEACH!</a><br /></p>
     </div>
--- Views/templates/navbarB.php
+++ Views/templates/navbarB.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-dark bg-dark">
 
     <a class="navbar-brand" href="/">TRAVEL THE WOLRD</a>
-    <a class="navbar-brand" href="/login">home</a>
+    <a class="navbar-brand" href="/login">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
